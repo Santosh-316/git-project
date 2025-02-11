e0218233620a28c042eaec85dd20e8b9bff234fe
santosh-316 <santoshch1999@gmail.com>
2025-02-11 16:57:21 +0530

    dev2 also

--- sanjay.java
+++ sanjay.java
@@ -1,10 +1,6 @@
 public class sanjay {
-    public void max()
-    {
-        System.out.println("haii");
-    }
+    
     public static void main(String[] args) {
-        sanjay sc = new sanjay();
-        sc.max();
+        System.out.println("kio");
     }
 }
