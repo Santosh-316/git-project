1a1d4fa3264a845b857a43e610bad94419754fa4
santosh-316 <santoshch1999@gmail.com>
2025-02-11 16:22:36 +0530

    new branch

---
+++ sanjay.java
@@ -0,0 +1,10 @@
+public class sanjay {
+    public void max()
+    {
+        System.out.println("haii");
+    }
+    public static void main(String[] args) {
+        sanjay sc = new sanjay();
+        sc.max();
+    }
+}
